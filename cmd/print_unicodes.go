86d87256e82df2d4d6259ac9acbf1f383f433657
nafiz1001 <nafiz.islam1001@gmail.com>
2021-07-31 15:29:42 -0400

    create print_unicodes.go

---
+++ cmd/print_unicodes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nafiz1001/french-card-go/card"
+	"github.com/nafiz1001/french-card-go/suit"
+)
+
+func main() {
+	fmt.Println("Printing suits...")
+	for s := suit.Club; s <= suit.Spade; s++ {
+		fmt.Printf("%s\t%c\n", s, s.Rune())
+	}
+
+	fmt.Println("Printing Cards...")
+	cards := card.CreateDeck()
+	for _, c := range cards {
+		fmt.Printf("%-16s\t%c\n", c, c.Rune())
+	}
+}
